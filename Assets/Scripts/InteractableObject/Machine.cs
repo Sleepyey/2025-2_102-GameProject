0351d7173a40cd760797fc83110dc2ed80934617
Sleepyey <jaeeyyun@gmail.com>
2025-09-25 12:03:38 +0900

    [Feat] 인터랙션 시스템

---
+++ Assets/Scripts/InteractableObject/Machine.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class Machine : InteractableObject
+{
+    protected override void Start()
+    {
+        base.Start();                                   // 기존 상속 받은 스타트 함수를 한번 실행 시킨다
+        objectName = "기계";
+        interactionText = "[E] 기계 동작";
+        interactionType = InteractionType.Machine;
+    }
+
+    protected override void OperateMachine()
+    {
+        StartCoroutine(DoOperateMachine());
+    }
+
+    IEnumerator DoOperateMachine()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            transform.Rotate(new Vector3(0, 1, 0), 30);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return new WaitForSeconds(0.1f);
+    }
+}
